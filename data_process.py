72e34983496c0064c908e3789746c4259c88999e
Rodian Fonseca Campuzano <rodian@pop-os.localdomain>
2020-07-11 17:21:34 -0500

    update clase de sitio

--- data_process.py
+++ data_process.py
@@ -23,11 +23,22 @@ neigh_data = pd.read_csv("barrios.csv")
 # Inner join with Neighbourhood data to filter
 df = pd.merge(df, neigh_data, how='inner', on='Barrio')
 
+# ===========
+# Clase sitio
+csitio = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9eDm9TuopO6ysMo4wvjNN2F26lb3zaYHCIMQH17g3utZ8xrkgU73VqebPnnJhY_jHJrOjhwzmyH1s/pub?gid=1999911815&single=true&output=csv"
+
+sitio_data = pd.read_csv(csitio)
+
+# Inner join with Neighbourhood data to filter
+df = pd.merge(df, sitio_data, how='left', on='Clase de sitio')
+
+# ======================
 # Drop unused columns
-df = df.drop(columns = ['Departamento', 'Barrio', 'Zona', 'Estado civil', 'Código DANE'])
+df = df.drop(columns = ['Departamento', 'Barrio', 'Zona', 'Estado civil', 'Código DANE','Clase de sitio'])
 
 # Rename Barrio column
-df = df.rename(columns={'Barrio_Nombre':'Barrio'})
+df = df.rename(columns={'Barrio_Nombre':'Barrio',
+			'Nueva clase':'Clase de sitio'})
 
 # Exporta data to csv
 df.to_csv('datos.csv', index=False, index_label=False)
